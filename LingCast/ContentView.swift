fe74a452d9a9e062ea2186e93dfeee8758f8cba1
ynyejin <yyj030625@gmail.com>
2026-09-11 14:39:46 +0900

    chore: initialize iOS project

---
+++ LingCast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LingCast
+//
+//  Created by 윤예진 on 9/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LingCast/LingCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingCastApp.swift
+//  LingCast
+//
+//  Created by 윤예진 on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LingCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
